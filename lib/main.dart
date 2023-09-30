c6ab0e534b4b0f6671f034242dc76b20a6bb032a
Zeck Lowe <lowezeck@gmail.com>
2023-09-30 09:11:51 +0800

    Finished setting up

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:course_app/screens/screens.dart';
 import 'package:flutter/material.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+//import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +13,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xff29274F),
       ),
+      home: const HomeScreen(),
     );
   }
 }
