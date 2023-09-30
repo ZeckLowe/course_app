a13b43510ee8878d7f2650c2111a7603a5d293b2
Zeck Lowe <lowezeck@gmail.com>
2023-09-30 18:03:14 +0800

    Added 2 text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,7 +14,35 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: Theme.of(context).primaryColor,
       body: SafeArea(
-          child: ListView(padding: const EdgeInsets.symmetric(horizontal: 20))),
+          child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        physics: const BouncingScrollPhysics(),
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Online',
+                  style: GoogleFonts.roboto(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  )),
+              Text('Master Class',
+                  style: GoogleFonts.roboto(
+                      fontSize: 36,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white))
+            ],
+          ),
+          const SizedBox(
+            height: 22,
+          ),
+          const SizedBox(
+            height: 349,
+          )
+        ],
+      )),
     );
   }
 }
